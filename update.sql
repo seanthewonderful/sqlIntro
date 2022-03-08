14a317704b3072e66f08108ea3952779c62cbff2
Sean <seanthewonderful@gmail.com>
2022-03-08 09:09:38 -0700

    tuesAM

--- update.sql
+++ update.sql
@@ -1,9 +1,19 @@
-SELECT * FROM customer;
+
 UPDATE customer
 SET fax = NULL;
 
-SELECT * FROM customer;
 UPDATE customer
 SET company = 'Self'
 WHERE company IS NULL;
 
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE last_name = 'Barnett';
+
+UPDATE customer
+SET support_rep_id = 5
+WHERE email = 'luisrojas@yahoo.cl';
+
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE genre_id = 3 AND composer IS NULL;
